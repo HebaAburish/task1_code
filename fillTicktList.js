ca383ebcdf95f88509ec053ae089fc9444a258c3
Heba Rish <heba@tickerchart.com>
2020-11-01 01:42:20 -0800

    commit 3

--- fillTicktList.js
+++ fillTicktList.js
@@ -1,8 +1,33 @@
+getList();
+function getList() {
 
 
-fillData();
-function fillData(){
-const ticketId = document.getElementById("ticktId").app;
-const problem =  document.getElementById("problem");
-ticketId.append("66");
-problem.append("heba");}
+    var xhr = new XMLHttpRequest();
+
+
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            myObj = JSON.parse(this.responseText);
+
+            for (i in myObj) {
+
+                var y = document.createElement("li");
+
+                var t = document.createTextNode("ticket"+ myObj[i].id);
+
+                y.appendChild(t);
+
+                document.getElementById("my-list").appendChild(y);
+            }
+
+
+        }
+        ;
+
+
+        xhr.open('GET', 'listAllUserTickets.php', false);
+        xhr.send();
+
+    }
+}
+
--- listAllUserTickets.php
+++ listAllUserTickets.php
@@ -4,16 +4,19 @@ include 'DBconnection.php';
 session_start();
 
 $connection= OpenCon();
-
+$ids=array([]);
 $query= "SELECT id,first_name,last_Name,user_name,email,phon_number,issue_summary,description FROM ticket";
 $result = $connection->query($query);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["first_name"]. " " . $row["last_Name"]. "<br>";
+
+           array_push($ids,$row["id"]);
+
     }
+    echo  json_encode($ids);
 } else {
-    echo "0 results";
+    echo 0;
 }
 $result ->close();
 CloseCon($connection);
